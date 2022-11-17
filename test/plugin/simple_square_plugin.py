cc4193057ad7ec6d92626d5cd1255c72a01b6cfb
amadeus <amsl@gmx.at>
2022-11-17 09:48:09 +0000

    remove parallel_plugin_collection and tests thereof

--- test/plugin/simple_square_plugin.py
+++
@@ -1,8 +0,0 @@
-from ivory.plugin.abstract_plugin import AbstractPlugin
-
-
-class SimpleSquarePlugin(AbstractPlugin):
-    """ Plugin that computes the square of ctx.value. """
-
-    def run(self):
-        self.ctx.value = self.ctx.value ** 2
