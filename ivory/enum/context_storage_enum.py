f306f30086ad5a0be61e3313a3b26344846074c9
amadeus <amsl@gmx.at>
2022-12-09 20:14:32 +0000

    rename the plugin_enum.py

---
+++ ivory/enum/context_storage_enum.py
@@ -0,0 +1,6 @@
+from enum import Enum, auto
+
+
+class ContextStorageEnum(Enum):
+    DIRECTORY = auto()
+    FILE_NAME = auto()
--- ivory/enum/plugin_enum.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum, auto
-
-
-class PluginEnum(Enum):
-    CONTEXT_STORAGE_DIRECTORY = auto()
-    CONTEXT_FILE_NAME = auto()
