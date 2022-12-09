fde957d564f4cce3acc5fe85273ae38d4889f3a8
amadeus <amsl@gmx.at>
2022-12-09 20:14:03 +0000

    add new config key

--- ivory/config_keys.py
+++ ivory/config_keys.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class ConfigKeys(Enum):
     """ Some often used configuration keys. """
+    PARAMS = 'params'
     PLUGINS = 'plugins'
     PIPELINE = 'Pipeline'
     CONTEXT_PROVIDER = 'context_provider'
