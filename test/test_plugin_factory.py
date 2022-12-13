fb7331d1a311c5b753e93a4f7c97de5779c830c7
amadeus <amsl@gmx.at>
2022-12-13 10:28:46 +0000

    instantiate plugins with their config directly, without handing over the context

--- test/test_plugin_factory.py
+++ test/test_plugin_factory.py
@@ -1,16 +1,52 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch, Mock
 
 import pytest
 
+from ivory.config_keys import ConfigKeys
 from ivory.context import ctx
 from ivory.exceptions.exceptions import UnsupportedPluginTypeException
 from ivory.plugin.plugin_factory import PluginFactory
+from ivory.utils.struct import Struct
 from test.plugin import simple_plugin
 
 PLUGIN_NAME = "test.plugin.simple_plugin"
 
 
 class TestPluginFactory:
+
+    @patch('ivory.plugin.plugin_factory.importlib')
+    @patch.object(PluginFactory, '_get_plugin_attribute')
+    def test_create_instance_when_context_empty(self, mock_get_plugin_attribute, mock_importlib):
+        plugin = PluginFactory.create_instance(plugin_name='plugin_name', ctx=Struct())
+        mock_importlib.import_module.assert_called_once_with('plugin_name')
+        mock_get_plugin_attribute.assert_called_once_with(mock_importlib.import_module())
+        mock_get_plugin_attribute().assert_called_once()
+        assert mock_get_plugin_attribute()() == plugin
+
+    @patch('ivory.plugin.plugin_factory.importlib')
+    @patch.object(PluginFactory, '_get_plugin_attribute')
+    def test_create_instance(self, mock_get_plugin_attribute, mock_importlib):
+        mock_config = {'mock': Mock()}
+        context = Struct({
+            ConfigKeys.PARAMS.value: {'plugin_name': mock_config},
+
+        })
+        mock_get_plugin_attribute.return_value.name = 'plugin_name'
+        plugin = PluginFactory.create_instance(plugin_name='plugin_name', ctx=context)
+        mock_importlib.import_module.assert_called_once_with('plugin_name')
+        mock_get_plugin_attribute.assert_called_once_with(mock_importlib.import_module())
+        mock_get_plugin_attribute().assert_called_once_with(**mock_config)
+        assert mock_get_plugin_attribute()() == plugin
+
+    def test_get_plugin_attribute_expect_none(self):
+        mock_module = Mock()
+        assert None == PluginFactory._get_plugin_attribute(module=mock_module)
+
+    def test_get_plugin_attribute(self):
+        mock_module = Mock()
+        mock_module.AbstractPlugin = Mock()  # this must be ignored
+        mock_module.MockPlugin = Mock()
+        assert mock_module.MockPlugin == PluginFactory._get_plugin_attribute(module=mock_module)
 
     def test_simple(self):
         plugin = PluginFactory.create_instance(PLUGIN_NAME, ctx())
