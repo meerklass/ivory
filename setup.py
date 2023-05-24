4df0c9a61b1de28306e59fb6e579421e4e8e72f8
amadeus <amsl@gmx.at>
2023-05-24 11:16:01 +0100

    new setup.py file

--- setup.py
+++ setup.py
@@ -1,76 +1,26 @@
-#!/usr/bin/env python
-
 import os
-import sys
 
-from setuptools import find_packages
-from setuptools.command.test import test as TestCommand
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
-
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
-
-
-readme = open('README.rst').read()
-doclink = """
-Documentation
--------------
-
-The full documentation can be generated with Sphinx"""
-
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
-
-requires = []
-tests_require = ['pytest>=2.3']
+from setuptools import setup, find_packages
 
 PACKAGE_PATH = os.path.abspath(os.path.join(__file__, os.pardir))
 
 setup(
     name='ivory',
     version='0.0.1',
-    description='Simple and flexible workflow engine',
-    long_description=readme + '\n\n' + doclink + '\n\n' + history,
+    description='Ivory: Simple and flexible workflow engine ',
     author='',
     author_email='',
-    url='',
     packages=find_packages(PACKAGE_PATH),
-    package_dir={'ivory': 'ivory'},
-    include_package_data=True,
-    install_requires=requires,
-    license='GPLv3',
-    zip_safe=False,
-    keywords='ivory',
-    entry_points={
-        'console_scripts': [
-            'ivory = ivory.cli.main:run',
-        ]
-    },
-
+    install_requires=[
+        'numpy~=1.23.3',
+        'py~=1.11.0',
+        'pytest~=7.1.3',
+        'setuptools~=59.6.0',
+        'joblib',
+        'ipyparallel',
+    ],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        "Intended Audience :: Science/Research",
-        'Intended Audience :: Developers',
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        'Natural Language :: English',
+        'GPLv3',
         'Programming Language :: Python :: 3.10',
     ],
-    tests_require=tests_require,
-    cmdclass={'test': PyTest},
 )
